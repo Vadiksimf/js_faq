01960492cf53308304d50f43d92da186607eae0a
vadym.h-ko <vadiksimf88@gmail.com>
2019-12-18 13:45:45 +0300

    Start made a game

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -19,20 +19,37 @@ scores = [0,0];
 roundScore = [0,0];
 activePlayer = 1;
 
-//Random numbers for 1 to 6
-dice = Math.floor(Math.random()*6)+1;
-console.log(dice);
-
-
 //Select DOM Object. Setting method
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 //Getting method - we get a valuable from HTML
-
-var x = document.querySelector('#current-1').textContent;
+/*
+var x = document.querySelector('#score-0').textContent;
 console.log(x);
+*/
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //1. Random number
+    //Random numbers for 1 to 6
+    dice = Math.floor(Math.random()*6)+1;
+
+    //2. Display the result
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src= 'dice-' + dice + '.png';
+
+    //3. Update the current score IF result is not 1.
+    document.getElementById('current-' + activePlayer).textContent = dice;
+
+})
 
 

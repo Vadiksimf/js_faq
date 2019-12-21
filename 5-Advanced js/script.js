bc0466f48c74f933b8852c0cf6726e6fa4382a90
vadym.h-ko <vadiksimf88@gmail.com>
2019-12-21 22:51:58 +0300

    next part

--- 5-Advanced js/script.js
+++ 5-Advanced js/script.js
@@ -1,5 +1,5 @@
-// Function constructor
-
+// Function constructor ***************************************
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
@@ -10,9 +10,6 @@ var Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-   /* this.calculateAge = function () {
-        console.log(2019 - this.yearOfBirth);
-    }*/
 };
 
 // Creating of prototype. The same method as in Person object
@@ -36,3 +33,90 @@ pete.calculateAge();
 console.log(mike.name +' '+ mike.lastName);
 console.log(mary.name +' '+ mary.lastName);
 console.log(pete.name +' '+ pete.lastName);
+*/
+
+
+// Object.create *******************************************
+/*
+
+var personProto = {
+    calculateAge: function () {
+        console.log(2019 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1989;
+john.job = 'programmer';
+
+var jane = Object.create(personProto, 
+    {
+        name: {value: 'Jane'},
+        yearOfBirth: {value: 1998},
+        job: {value: 'designer'}
+    }
+    );
+*/
+
+
+// Primitives vs objects
+
+
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+//Objects
+var obj1 = {
+    name: 'Pete',
+    age: 31
+};
+
+var obj2 = obj1;
+
+obj1.age = 30;
+
+console.log(obj1);
+console.log(obj2);
+
+
+
+//Functions
+
+var age = 27;
+var obj = {
+    name: 'Peter',
+    city: 'Warshaw'
+};
+
+function change(a,b) {
+    a = 30;
+    b.city = 'London';
+};
+
+change(age,obj);
+
+console.log(age);
+console.log(obj.city);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
